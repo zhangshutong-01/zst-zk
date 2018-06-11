16008d40ea4f65379e39636d9bef830fec631d19
zhangshutong <937784097@qq.com>
2018-06-11 09:13:43 +0800

    项目开发环境构建

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,25 @@
 var gulp = require('gulp');
-var server = require('gulp-server');
-var
+var server = require('gulp-webserver');
+var minCss = require('gulp-clean-css');
+var scss = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var path = require('path');
+var url = require('url');
+var fs = require('fs')
+gulp.task('server', function() {
+    gulp.src('src')
+        .pipe(server({
+            livereload: true,
+            port: 8080,
+            open: true,
+            middleware: function(req, res, next) {
+                var pathname = url.parse(req.url).pathname;
+                //  console.log(pathname)
+                if (pathname === '/favicon.ico') {
+                    return false
+                }
+                pathname = '/' ? '/index.html' : pathname;
+                fs.readFileSync(path.join(__dirname, 'src', pathname))
+            }
+        }));
+})
